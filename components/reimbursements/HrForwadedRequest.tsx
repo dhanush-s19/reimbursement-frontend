eae7e2e50e3c3baa9175777d71fa8a85354535ce
Dhanush <dhanushfullstack19@gmail.com>
2026-04-14 15:20:59 +0530

    fixed-pagination-hr

--- components/reimbursements/HrForwadedRequest.tsx
+++ components/reimbursements/HrForwadedRequest.tsx
@@ -181,15 +181,17 @@ export default function HRReimbursementList({ hrId }: Readonly<Props>) {
             onRowClick={handleRowClick}
           />
         </div>
-        {totalPages > 1 && (
-          <div className="mt-8 flex justify-center">
-            <Pagination
-              currentPage={page}
-              totalPages={totalPages}
-              onPageChange={setPage}
-              loading={loading}
-            />
-          </div>
+        {totalPages > 0 && (
+          <footer className="fixed bottom-0 left-0 right-0 bg-white/80 backdrop-blur-md border-t border-gray-200 py-4 shadow-[0_-4px_20px_-5px_rgba(0,0,0,0.05)] z-10">
+                <div className="flex justify-center items-center">
+                    <Pagination
+                        currentPage={page}
+                        totalPages={totalPages}
+                        onPageChange={setPage}
+                        loading={loading}
+                    />
+                </div>
+            </footer>
         )}
       </div>
     </div>
